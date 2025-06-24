041ceee618158a70b9b53a3f93e7e4d5f2f807a9
Takuya0202 <shimozono.t.24kdgn@gmail.com>
2025-06-24 12:43:24 +0900

    station2までクリア？

--- src/index.jsx
+++ src/index.jsx
@@ -1,26 +1,36 @@
-import React, { StrictMode } from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
-import { Provider } from 'react-redux'
-import { store } from './store/index'
-import axios from '~/vendor/axios'
+import React, { StrictMode } from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { Provider } from "react-redux";
+import { store } from "./store/index";
+import axios from "~/vendor/axios";
+import { createRoot } from "react-dom/client";
 
-axios.interceptors.request.use(config => {
-  const token = store.getState().auth.token
+axios.interceptors.request.use((config) => {
+  const token = store.getState().auth.token;
   if (token) {
-    config.headers.Authorization = `Bearer ${token}`
+    config.headers.Authorization = `Bearer ${token}`;
   }
 
-  return config
-})
+  return config;
+});
 
-const root = document.getElementById('root')
-ReactDOM.render(
+// const root = document.getElementById('root')
+// ReactDOM.render(
+//   <StrictMode>
+//     <Provider store={store}>
+//       <App />
+//     </Provider>
+//   </StrictMode>,
+//   root,
+// )
+
+// 書き換え
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
   </StrictMode>,
-  root,
-)
+);
