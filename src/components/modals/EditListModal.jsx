c1a5f5efcaab37360b7f860abf79c607d93db058
Takuya0202 <shimozono.t.24kdgn@gmail.com>
2025-07-14 17:31:38 +0900

    fix modal

--- src/components/modals/EditListModal.jsx
+++ src/components/modals/EditListModal.jsx
@@ -5,6 +5,8 @@ import { useState , useEffect} from 'react';
 import { useSelector } from 'react-redux';
 import { fetchLists, updateList, deleteList } from '~/store/list/index';
 import Modal from 'react-modal';
+import { Box, Button, TextField, Typography } from '@mui/material';
+import DeleteIcon from '@mui/icons-material/Delete';
 
 
 
@@ -77,17 +79,70 @@ const EditListModal = ({isOpen , setIsOpen , listId}) => {
       contentLabel="Edit List Modal"
       style={{
         content:{
-          maxWidth: '500px',
+          top: '50%',
+          left: '50%',
+          right: 'auto',
+          bottom: 'auto',
+          transform: 'translate(-50%, -50%)',
+          width: '500px',
+          height: 'auto',
           margin: 'auto',
           padding: '2rem',
           borderRadius: '8px',
         },
         overlay : {
-          backgroundColor: 'rgba(0, 0, 0, 0.75)',
+          backgroundColor: 'rgba(0, 0, 0, 0.8)',
           zIndex: 1000,
         }
       }}>
-
+        <Typography variant='h4' component={"h2"} align='left' color='#3e3e3e' fontWeight={'500'} mb={2}>
+          Edit List name
+        </Typography>
+        <Typography variant='p' component={"p"} align='left' color='error'>
+          {errorMessage}
+        </Typography>
+        <Box component="form" onSubmit={onSubmit}>
+          <TextField 
+            label="name"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            fullWidth
+          />
+        </Box>
+        <Box component={"div"} 
+          style={{display:'flex',
+          justifyContent:'space-between',
+          alignItems:'center',
+          widowth:'80%',
+          margin: '1rem auto ',
+           }}>
+          <Button variant='outlined' onClick={onclose} type='button'>
+            Cancel
+          </Button>
+          <Box component={"div"}
+            style={{
+              display: 'flex',
+              alignItems: 'center',
+              gap: '1rem',
+            }}
+          >
+            <Button
+              type='button'
+              variant='contained'
+              color='secondary'
+              disabled={isSubmitting}
+              onClick={handleDelete}
+              startIcon={<DeleteIcon />}>
+                Delete
+            </Button>
+            <Button
+              type='submit'
+              variant='contained'
+              disabled={isSubmitting}>
+                Update
+            </Button>
+          </Box>
+        </Box>
     </Modal>
   )
 }
